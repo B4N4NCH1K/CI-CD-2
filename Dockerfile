85130c8d157696f79f86b6c67e7efb020c240efb
B4N4NCH1K <150067653+B4N4NCH1K@users.noreply.github.com>
2024-10-30 14:21:23 +0500

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM python:3.9.2
 RUN pip install flask
 WORKDIR /code
 COPY web.py /code/web.py
+COPY static/ /code/static 
 EXPOSE 5000
 CMD ["python3", "web.py"]
--- web.py
+++ web.py
@@ -4,7 +4,14 @@ web=Flask(__name__)
 
 @web.route("/healthz")
 def ok():
-    return 'OHIO'
+    return '''
+    <html>
+        <body>
+            <h1>In case I don't see ya, good afternoon, good evening, and good night!</h1>
+            <img src="static/truman.png" width="640" height="360">
+        </body>
+    </html>
+    '''
 
 if __name__=="__main__":
     web.run(host="0.0.0.0", port="5555")
